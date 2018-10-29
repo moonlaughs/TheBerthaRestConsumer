3c16cca94f5d2b9790478100bf976028378d1040
Milena Ognyanova <mile0416@edu.easj.dk>
2018-10-29 14:02:23 +0100

    should work get

--- TheBerthaRestConsumer/Controllers/EnvironmentController.cs
+++ TheBerthaRestConsumer/Controllers/EnvironmentController.cs
@@ -18,7 +18,7 @@ namespace TheBerthaRestConsumer.Controllers
         [HttpGet]
         public IEnumerable<Model.Environment> Get()
         {
-            string selectString = "select * from Environment";
+            string selectString = "select * from EnvironmentData";
             using (SqlConnection conn = new SqlConnection(connectionString))
             {
                 conn.Open(); 
@@ -45,24 +45,25 @@ namespace TheBerthaRestConsumer.Controllers
             int id = reader.GetInt32(0);
             string oxygen = reader.IsDBNull(1) ? null : reader.GetString(1); ;
             string co2 = reader.IsDBNull(2) ? null : reader.GetString(2);
-            string pm25 =
-                reader.IsDBNull(3) ? null : reader.GetString(3);
-            string pm10 = reader.IsDBNull(4) ? null : reader.GetString(4);
-            string ozone = reader.IsDBNull(5) ? null : reader.GetString(5);
-            string dustParticles = reader.IsDBNull(6) ? null : reader.GetString(6);
-            string nitrogenDioxide = reader.IsDBNull(7) ? null : reader.GetString(7);
-            string sulphurDioxide = reader.IsDBNull(8) ? null : reader.GetString(8);
-            int userId = reader.GetInt32(9);
-            DateTime dateTime = reader.GetDateTime(10);
+            string co = reader.IsDBNull(3) ? null : reader.GetString(3);
+            string pm25 = reader.IsDBNull(4) ? null : reader.GetString(4);
+            string pm10 = reader.IsDBNull(5) ? null : reader.GetString(5);
+            string ozon = reader.IsDBNull(6) ? null : reader.GetString(6);
+            string dustParticles = reader.IsDBNull(7) ? null : reader.GetString(7);
+            string nitrogenDioxide = reader.IsDBNull(8) ? null : reader.GetString(8);
+            string sulphurDioxide = reader.IsDBNull(9) ? null : reader.GetString(9);
+            int userId = reader.GetInt32(10);
+            DateTime dateTime = reader.GetDateTime(11);
 
             Model.Environment env = new Model.Environment()
             {
                 Id = id,
                 Oxygen = oxygen,
                 Co2 = co2,
+                Co = co,
                 Pm25 = pm25,
                 Pm10 = pm10,
-                Ozone = ozone,
+                Ozon = ozon,
                 DustParticles = dustParticles,
                 NitrogenDioxide = nitrogenDioxide,
                 SulphurDioxide = sulphurDioxide,
@@ -115,7 +116,7 @@ namespace TheBerthaRestConsumer.Controllers
                     command.Parameters.AddWithValue("@co2", value.Co2);
                     command.Parameters.AddWithValue("@pm25", value.Pm25);
                     command.Parameters.AddWithValue("@pm10", value.Pm10);
-                    command.Parameters.AddWithValue("@ozone", value.Ozone);
+                    command.Parameters.AddWithValue("@ozone", value.Ozon);
                     command.Parameters.AddWithValue("@dustParticles", value.DustParticles);
                     command.Parameters.AddWithValue("@nitrogenDioxide", value.NitrogenDioxide);
                     command.Parameters.AddWithValue("@sulphirDioxide", value.SulphurDioxide);
@@ -144,7 +145,7 @@ namespace TheBerthaRestConsumer.Controllers
                     updateCommand.Parameters.AddWithValue("@co2", value.Co2);
                     updateCommand.Parameters.AddWithValue("@pm25", value.Pm25);
                     updateCommand.Parameters.AddWithValue("@pm10", value.Pm10);
-                    updateCommand.Parameters.AddWithValue("@ozone", value.Ozone);
+                    updateCommand.Parameters.AddWithValue("@ozone", value.Ozon);
                     updateCommand.Parameters.AddWithValue("@dustParticles", value.DustParticles);
                     updateCommand.Parameters.AddWithValue("@nitrogenDioxide", value.NitrogenDioxide);
                     updateCommand.Parameters.AddWithValue("@sulphirDioxide", value.SulphurDioxide);
--- TheBerthaRestConsumer/Model/Environment.cs
+++ TheBerthaRestConsumer/Model/Environment.cs
@@ -11,23 +11,25 @@ namespace TheBerthaRestConsumer.Model
         public int Id { get; set; }
         public string Oxygen { get; set; }
         public string Co2 { get; set; }
+        public string Co { get; set; }
         public string Pm25 { get; set; }
         public string Pm10 { get; set; }
-        public string Ozone { get; set; }
+        public string Ozon { get; set; }
         public string DustParticles { get; set; }
         public string NitrogenDioxide { get; set; }
         public string SulphurDioxide { get; set; }
         public int UserId { get; set; }
         public DateTime DateTime { get; set; }
 
-        public Environment(int id, string oxygen, string co2, string pm25, string pm10, string ozone, string dustParticles, string nitrogenDioxide, string sulphurDioxide, int userId, DateTime dateTime)
+        public Environment(int id, string oxygen, string co2,string co, string pm25, string pm10, string ozon, string dustParticles, string nitrogenDioxide, string sulphurDioxide, int userId, DateTime dateTime)
         {
             Id = id;
             Oxygen = oxygen;
             Co2 = co2;
+            Co = co;
             Pm25 = pm25;
             Pm10 = pm10;
-            Ozone = ozone;
+            Ozon = ozon;
             DustParticles = dustParticles;
             NitrogenDioxide = nitrogenDioxide;
             SulphurDioxide = sulphurDioxide;
@@ -42,7 +44,7 @@ namespace TheBerthaRestConsumer.Model
 
         public override string ToString()
         {
-            return Id + Oxygen + Co2 + Pm25 + Pm10 + DustParticles + NitrogenDioxide + SulphurDioxide + UserId + DateTime;
+            return Id + Oxygen + Co2 + Co + Pm25 + Pm10 + Ozon + DustParticles + NitrogenDioxide + SulphurDioxide + UserId + DateTime;
         }
     }
 }
